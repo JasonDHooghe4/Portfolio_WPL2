6eb8ca9241ab785004327802458477475fdf8bd9
JasonD'HooghePXL <12302919@student.pxl.be>
2024-06-06 21:30:54 +0200

    Projects section aangepast en beetje WPL2 inhoud toegevoegd.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ function filterImages(category) {
         }
     });
 }
+
+function WPL1() {
+    document.getElementById('WPL1').style.display = 'block';
+    document.getElementById('WPL2').style.display = 'none';
+}
+
+function WPL2() {
+    document.getElementById('WPL1').style.display = 'none';
+    document.getElementById('WPL2').style.display = 'block';
+}
+document.addEventListener('DOMContentLoaded', (event) => {
+    WPL2();
+});

43e6f5efad457e445ecd4f6d8496e9e4ed71ff29
JasonD'HooghePXL <12302919@student.pxl.be>
2024-06-07 23:12:23 +0200

    Foto's toegevoegd aan projecten

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-function filterImages(category) {
-    const images = document.querySelectorAll('.filterable');
-
-    images.forEach((image) => {
-        const imageCategory = image.getAttribute('data-category');
-
-        if (category === 'all' || category === imageCategory) {
-            image.parentNode.style.display = 'block';
-        } else {
-            image.parentNode.style.display = 'none';
-        }
-    });
-}
-
 function WPL1() {
     document.getElementById('WPL1').style.display = 'block';
     document.getElementById('WPL2').style.display = 'none';
